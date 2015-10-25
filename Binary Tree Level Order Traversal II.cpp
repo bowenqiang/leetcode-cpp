7dbb00b33c1c64ba41337326f5e187528250b35a
bowenqiang <bowenqiang@live.com>
2015-10-25 16:49:43 -0500

    solved 10/25/2015

---
+++ Binary Tree Level Order Traversal II.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>>result;
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        function(root,0);
+        return vector<vector<int>>(result.rbegin(),result.rend());
+        
+    }
+    void function(TreeNode* root,int level)
+    {
+        if(root==NULL)
+            return;
+        if(result.size()>level)
+        {
+            result[level].push_back(root->val);
+        }
+        else
+        {
+            vector<int>temp;
+            temp.push_back(root->val);
+            result.push_back(temp);
+        }
+        
+        function(root->left,level+1);
+        function(root->right,level+1);   
+        
+        
+    }
+};
