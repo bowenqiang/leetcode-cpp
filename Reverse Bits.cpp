c8ec0c4df0d7a9062d692dd9354e12a7fd9636a9
Wenqiang Bo <bowenqiang@live.com>
2015-10-27 15:06:30 -0500

    Create Reverse Bits.cpp

---
+++ Reverse Bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t m=0;
+        for(int i=0;i<32;i++)
+        {
+            m*=2;
+            m+=n%2;
+            n/=2;
+        }
+    return m;
+        
+    }
+};
