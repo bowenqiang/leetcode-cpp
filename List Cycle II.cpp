3bf1b081447c99043de801016465c9180af591cc
Wenqiang Bo <bowenqiang@live.com>
2015-10-29 15:58:40 -0500

    Create List Cycle II.cpp
    Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
    
    Note: Do not modify the linked list.

---
+++ List Cycle II.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head==NULL)
+        {
+            return NULL;
+        }
+            ListNode *fast,*slow;
+            fast=head;
+            slow=head;
+            do
+            {
+                if(fast->next==NULL || fast->next->next==NULL)
+                    return NULL;
+                fast=fast->next->next;
+                slow=slow->next;
+            }while(fast!=slow);
+        slow=head;
+        while(slow!=fast)
+        {
+            slow=slow->next;
+            fast=fast->next;
+        }
+        return slow;
+        
+    }
+};
