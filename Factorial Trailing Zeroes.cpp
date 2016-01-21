b1f16ccfea0abb16e344f4e01fa9e6edb32d0244
Wenqiang Bo <bowenqiang@live.com>
2016-01-20 17:04:55 -0800

    Update Factorial Trailing Zeroes.cpp
    Given an integer n, return the number of trailing zeroes in n!.

--- Factorial Trailing Zeroes.cpp
+++ Factorial Trailing Zeroes.cpp
@@ -1,23 +1,12 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
-    bool hasPathSum(TreeNode* root, int sum) {
-        if(root==NULL)
-            return false;
-        //if(root->val>sum)
-            //return false;
-        if(root->val==sum && root->right==NULL && root->left==NULL)
-            return true;
-        
-        
-        return hasPathSum(root->left,sum-root->val)+hasPathSum(root->right,sum-root->val);
+    int trailingZeroes(int n) {
+        int res=0;
+        while(n)
+        {
+            res+=n/5;
+            n/=5;
+        }
+    return res;
     }
 };
