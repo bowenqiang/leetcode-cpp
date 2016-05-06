5a38c573701343ffd93f99386b35b68a0109ecb4
Wenqiang Bo <bowenqiang@live.com>
2016-05-05 17:46:33 -0700

    Create Subsets II_slow.cpp
    Given a collection of integers that might contain duplicates, nums, return all possible subsets.
    
    Note:
    Elements in a subset must be in non-descending order.
    The solution set must not contain duplicate subsets.
    For example,
    If nums = [1,2,2], a solution is:
    
    [
      [2],
      [1],
      [1,2,2],
      [2,2],
      [1,2],
      []
    ]

---
+++ Subsets II_slow.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        vector<int> result;
+        backtracking(ans,result,nums,0);
+        return ans;
+        
+    }
+    void backtracking(vector<vector<int>> &ans,vector<int> result,vector<int>nums,int idx)
+    {
+        if(idx==nums.size()+1)
+            return;
+        if(isDup(ans,result))
+        {
+            return;
+        }
+        ans.push_back(result);
+        for(int i=idx;i<nums.size();i++)
+        {
+            result.push_back(nums[i]);
+            backtracking(ans,result,nums,i+1);
+            result.pop_back();
+        }
+    }
+    bool isDup(vector<vector<int>> &ans,vector<int> result)
+    {
+        for(int i=0;i<ans.size();i++)
+        {
+            if(ans[i]==result)
+                return true;
+        }
+        return false;
+    }
+    
+};
