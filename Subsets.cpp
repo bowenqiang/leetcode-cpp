736ea94023f916d412e27a58ed753d0412c2f915
Wenqiang Bo <bowenqiang@live.com>
2016-05-05 17:39:49 -0700

    Create Subsets.cpp
    Given a set of distinct integers, nums, return all possible subsets.
    
    Note:
    Elements in a subset must be in non-descending order.
    The solution set must not contain duplicate subsets.
    For example,
    If nums = [1,2,3], a solution is:
    
    [
      [3],
      [1],
      [2],
      [1,2,3],
      [1,3],
      [2,3],
      [1,2],
      []
    ]

---
+++ Subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        vector<int> result;
+        backtracking(ans,result,nums,0);
+        return ans;
+        
+    }
+    void backtracking(vector<vector<int>> &ans,vector<int> result,vector<int>nums,int idx)
+    {
+        if(idx==nums.size()+1)
+            return;
+        ans.push_back(result);
+        for(int i=idx;i<nums.size();i++)
+        {
+            result.push_back(nums[i]);
+            backtracking(ans,result,nums,i+1);
+            result.pop_back();
+        }
+    }
+    
+};
