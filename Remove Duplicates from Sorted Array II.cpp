15400df8b009f7b685d51a27151a005374f08c11
Wenqiang Bo <bowenqiang@live.com>
2016-06-07 14:38:30 -0700

    Create Remove Duplicates from Sorted Array II.cpp
    Follow up for "Remove Duplicates":
    What if duplicates are allowed at most twice?
    
    For example,
    Given sorted array nums = [1,1,1,2,2,3],
    
    Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.

---
+++ Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size()<=2)
+            return nums.size();
+        int count=2;
+        int temp=nums[2];
+        for(int i=2;i<nums.size();i++)
+        {
+            if(nums[i]!=nums[count-2])
+            {
+                nums[count++]=nums[i];
+            }
+            
+        }
+        return count;
+
+    }
+};
