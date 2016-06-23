0accb6ae96931c140214b9f8e247e499e8b458d7
Wenqiang Bo <bowenqiang@live.com>
2016-06-22 17:14:46 -0700

    Update Rotate Image.cpp

--- Rotate Image.cpp
+++ Rotate Image.cpp
@@ -14,3 +14,27 @@ class Solution {
         }
     }
 };
+/*
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int row=0;row<n;row++)
+        {
+            for(int col=row+1;col<n;col++)
+            {
+                swap(matrix[row][col],matrix[col][row]);
+            }
+        }
+        for(int i=0,j=n-1;i<j;i++,j--)
+        {
+            for(int k=0;k<n;k++)
+            {
+                swap(matrix[k][i],matrix[k][j]);
+            }
+        }
+        
+        
+    }
+};
+*/
