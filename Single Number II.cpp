79d903418b7cbe994b717c047eed9e1c262331b0
Wenqiang Bo <bowenqiang@live.com>
2016-06-23 16:21:12 -0700

    Update Single Number II.cpp

--- Single Number II.cpp
+++ Single Number II.cpp
@@ -14,3 +14,23 @@ public:
         return res;
     }
 };
+/*
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int one=0;
+        int two=0;
+        int three=0;
+        for(auto i:nums)
+        {
+            two|=(one & i);
+            one^=i;
+            three=~(one & two);
+            one&=three;
+            two&=three;
+        }
+        return one;
+        
+    }
+};
+*/
