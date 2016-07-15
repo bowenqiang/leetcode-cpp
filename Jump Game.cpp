ac2f0cc1b4ec81e505ab25ac8cf28efec97229b5
Wenqiang Bo <bowenqiang@live.com>
2016-07-15 16:17:54 -0700

    Update Jump Game.cpp

--- Jump Game.cpp
+++ Jump Game.cpp
@@ -17,3 +17,16 @@ public:
     }
 
 };
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reach=1;
+        for(int i=0;i<reach && reach<nums.size();i++)
+        {
+            reach=max(reach,i+nums[i]+1);
+        }
+        return reach>=nums.size();
+        
+    }
+};
